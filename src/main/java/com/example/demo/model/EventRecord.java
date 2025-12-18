ab27a71dd5f7e42925734f4f00fb433fe024d912
bala-amypo <bala@amypo.in>
2025-12-18 16:43:03 +0000

    User commit

--- src/main/java/com/example/demo/model/EventRecord.java
+++ src/main/java/com/example/demo/model/EventRecord.java
@@ -70,12 +70,14 @@ public class EventRecord{
     }
 
     public EventRecord(){};
-    public EventRecord(Long id, String eventCode, String eventName,String venue,Date eventDate,double basePrice;,Date createdAt;
-    private Boolean active){
+    public EventRecord(Long id, String eventCode, String eventName,String venue,Date eventDate,double basePrice,Date createdAt,Boolean active){
         this.id=id;
-        this.name=name;
-        this.username=username;
-        this.password=password;
-        this.created=created;
+        this.eventCode=eventCode;
+        this.eventName=eventName;
+        this.venue=venue;
+        this.eventDate=eventDate;
+        this.basePrice=basePrice;
+        this.createdAt=createdAt;
+        this.active=active;
     }
 }
