d6b0fbc1158a516f1fb84d59c3bee7fe40256a71
bala-amypo <bala@amypo.in>
2025-12-20 17:11:47 +0000

    User commit

--- src/main/java/com/example/demo/model/SeatInventoryRecord.java
+++ src/main/java/com/example/demo/model/SeatInventoryRecord.java
@@ -16,7 +16,7 @@ public class SeatInventoryRecord {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long eventId; // foreign key to EventRecord
+    private Long eventId; 
     private Integer totalSeats;
     private Integer remainingSeats;
     private LocalDateTime updatedAt;
