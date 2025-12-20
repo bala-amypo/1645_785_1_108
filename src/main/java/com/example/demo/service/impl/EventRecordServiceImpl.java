ab1628d76a5e6517243883d7410238a48ff52625
bala-amypo <bala@amypo.in>
2025-12-20 11:14:47 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/EventRecordServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/EventRecordServiceImpl.java
@@ -1,21 +1,21 @@
-// package com.example.demo.service.impl;
+// // package com.example.demo.service.impl;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
-// import com.example.demo.service.EventRecordService;
-// import com.example.demo.repository.EventRecordRepository;
-// import com.example.demo.model.EventRecord;
-// import java.util.*;
+// // import org.springframework.beans.factory.annotation.Autowired;
+// // import org.springframework.stereotype.Service;
+// // import com.example.demo.service.EventRecordService;
+// // import com.example.demo.repository.EventRecordRepository;
+// // import com.example.demo.model.EventRecord;
+// // import java.util.*;
 
-// @Service
-// public class EventRecordServiceImpl implements EventRecordService{
-//     @Autowired EventRecordRepository eventR;
+// // @Service
+// // public class EventRecordServiceImpl implements EventRecordService{
+// //     @Autowired EventRecordRepository eventR;
 //     @Override
-//     public EventRecord postData(EventRecord eve){
-//         return eventR.save(eve);
-//     }
-//     @Override
-//     public List<EventRecord>getAllData(){
-//         return eventR.findAll();
-//     }
-// }
+// //     public EventRecord postData(EventRecord eve){
+// //         return eventR.save(eve);
+// //     }
+// //     @Override
+// //     public List<EventRecord>getAllData(){
+// //         return eventR.findAll();
+// //     }
+// // }
