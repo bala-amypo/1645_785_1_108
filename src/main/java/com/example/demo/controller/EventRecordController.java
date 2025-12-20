dcf066a802d0370db15251ae239f850bd96d1a00
bala-amypo <bala@amypo.in>
2025-12-20 16:29:47 +0000

    User commit

--- src/main/java/com/example/demo/controller/EventRecordController.java
+++ src/main/java/com/example/demo/controller/EventRecordController.java
@@ -7,7 +7,7 @@ import com.example.demo.service.EventRecordService;
 
 @RestController
 public class EventRecordController {
-    @Autowired EventRecordService service;
+    private EventRecordService service;
     public EventRecordController(EventRecordService service) {
         this.service = service;
     }
