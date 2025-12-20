92264d797b5dbff74f78be8ae241fb0769604600
bala-amypo <bala@amypo.in>
2025-12-20 10:02:47 +0000

    User commit

--- src/main/java/com/example/demo/controller/DynamicPricingController.java
+++ src/main/java/com/example/demo/controller/DynamicPricingController.java
@@ -2,4 +2,5 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import com.example.demo.model.DynamicPriceRecord;
-import com.example.demo.service.DynamicPriceRecord
+import com.example.demo.service.DynamicPriceRecordService;
+import com.example.demo.service
