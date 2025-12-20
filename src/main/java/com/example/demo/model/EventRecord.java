314e5090f69b709f9f1a7788eeb024af9644e54c
bala-amypo <bala@amypo.in>
2025-12-20 16:44:48 +0000

    User commit

--- src/main/java/com/example/demo/model/EventRecord.java
+++ src/main/java/com/example/demo/model/EventRecord.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-
+import jakarta.persistence.PrePersist;
 @Entity
 public class EventRecord{
     @Id
