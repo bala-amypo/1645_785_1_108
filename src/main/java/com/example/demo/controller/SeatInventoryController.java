9f45e35f81f9b9b032ff41c5b502dbcf8ae87cbb
bala-amypo <bala@amypo.in>
2025-12-25 17:34:28 +0000

    User commit

--- src/main/java/com/example/demo/controller/SeatInventoryController.java
+++ src/main/java/com/example/demo/controller/SeatInventoryController.java
@@ -5,21 +5,23 @@ import java.util.List;
 import com.example.demo.model.SeatInventoryRecord;
 import com.example.demo.service.SeatInventoryService;
 
+
+@RequestMapping("/inventory")
 @RestController
 public class SeatInventoryController {
     private SeatInventoryService service;
     public SeatInventoryController(SeatInventoryService service){
         this.service = service;
     }
-    @PostMapping("/inventory/")
+    @PostMapping("/")
     public SeatInventoryRecord createInventory(@RequestBody SeatInventoryRecord inventory){
         return service.createInventory(inventory);
     }
-    @PutMapping("/inventory/{eventId}/remaining")
+    @PutMapping("/{eventId}/remaining")
     public SeatInventoryRecord updateRemainingSeats(@PathVariable Long eventId,@RequestParam Integer remainingSeats){
         return service.updateRemainingSeats(eventId, remainingSeats);
     }
-    @GetMapping("/inventory/event/{eventId}")
+    @GetMapping("/event/{eventId}")
     public SeatInventoryRecord getInventoryByEvent(@PathVariable Long eventId){
         return service.getInventoryByEvent(eventId);
     }
