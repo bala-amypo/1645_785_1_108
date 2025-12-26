7212bb9fa70173803df9c46f2694c529f79cfac8
bala-amypo <bala@amypo.in>
2025-12-26 14:17:39 +0000

    User commit

--- src/main/java/com/example/demo/servlet/HelloServlet.java
+++ src/main/java/com/example/demo/servlet/HelloServlet.java
@@ -1,35 +1,36 @@
-package com.example.demo.servlet;
-
-import java.io.IOException;
-
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-// @WebServlet("/hello-servlet")
-public class HelloServlet extends HttpServlet{
-    @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-        res.setStatus(200);
-        res.setContentType("text/plain");
-        res.getWriter().write("HelloMessage");
-     
-    }
-}
 // package com.example.demo.servlet;
 
+// import java.io.IOException;
+
+// import jakarta.servlet.annotation.WebServlet;
 // import jakarta.servlet.http.HttpServlet;
 // import jakarta.servlet.http.HttpServletRequest;
 // import jakarta.servlet.http.HttpServletResponse;
 
-// import java.io.IOException;
-
-// public class HelloServlet extends HttpServlet {
-
+// @WebServlet("/hello-servlet")
+// public class HelloServlet extends HttpServlet{
 //     @Override
-//     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-//         resp.setContentType("text/plain");
-//         resp.getWriter().write("HelloMessage");
+//     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+//         res.setStatus(200);
+//         res.setContentType("text/plain");
+//         res.getWriter().write("Hello from Dynamic Event Ticket Pricing Servlet");
+     
 //     }
 // }
+package com.example.demo.servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setStatus(HttpServletResponse.SC_OK);
+        res.setContentType("text/plain");
+        res.getWriter().write("Hello from Dynamic Event Ticket Pricing Servlet");
+    }
+}
