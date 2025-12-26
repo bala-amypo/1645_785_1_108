c24c2ca6807c0776d85c7b176b9d6fbb59db5415
bala-amypo <bala@amypo.in>
2025-12-26 17:39:33 +0000

    User commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,24 +1,9 @@
-// package com.example.demo.service;
-
-// import com.example.demo.model.User;
-// import java.util.List;
-
-// public interface UserService {
-//     User save(User user);
-//     User findByEmail(String email);
-//     List<User> getAllUsers();
-//     User getUserById(Long id);
-//     // boolean existsByEmail(String name);
-// }
 package com.example.demo.service;
 
 import java.util.Optional;
-
 import com.example.demo.model.User;
 
 public interface UserService {
-
     Optional<User> findByEmail(String email);
-
     boolean existsByEmail(String email);
 }
--- src/main/java/com/example/demo/servlet/HelloServlet.java
+++ src/main/java/com/example/demo/servlet/HelloServlet.java
@@ -1,22 +1,3 @@
-// package com.example.demo.servlet;
-
-// import java.io.IOException;
-
-// import jakarta.servlet.annotation.WebServlet;
-// import jakarta.servlet.http.HttpServlet;
-// import jakarta.servlet.http.HttpServletRequest;
-// import jakarta.servlet.http.HttpServletResponse;
-
-// @WebServlet("/hello-servlet")
-// public class HelloServlet extends HttpServlet{
-//     @Override
-//     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-//         res.setStatus(200);
-//         res.setContentType("text/plain");
-//         res.getWriter().write("Hello from Dynamic Event Ticket Pricing Servlet");
-     
-//     }
-// }
 package com.example.demo.servlet;
 
 import java.io.IOException;
