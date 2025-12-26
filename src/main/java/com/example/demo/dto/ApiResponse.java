0d95555ecfbd51dde54cb916ff71b7a1512909bc
bala-amypo <bala@amypo.in>
2025-12-26 18:00:34 +0000

    User commit

--- src/main/java/com/example/demo/dto/ApiResponse.java
+++ src/main/java/com/example/demo/dto/ApiResponse.java
@@ -1,52 +1,14 @@
-// package com.example.demo.dto;
-
-// import lombok.AllArgsConstructor;
-// import lombok.Data;
-
-// @Data
-// @AllArgsConstructor
-// public class ApiResponse {
-//     private boolean success;
-//     private String message;
-//     private Object data;
-// }
 package com.example.demo.dto;
 
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ApiResponse {
-
- private boolean success;
- private String message;
- private Object data;
-
- public ApiResponse() {}
-
- public ApiResponse(boolean success, String message, Object data) {
-  this.success = success;
-  this.message = message;
-  this.data = data;
- }
-
- public boolean isSuccess() {
-  return success;
- }
-
- public void setSuccess(boolean success) {
-  this.success = success;
- }
-
- public String getMessage() {
-  return message;
- }
-
- public void setMessage(String message) {
-  this.message = message;
- }
-
- public Object getData() {
-  return data;
- }
-
- public void setData(Object data) {
-  this.data = data;
- }
+    private boolean success;
+    private String message;
+    private Object data;
 }
--- src/main/java/com/example/demo/dto/AuthRequest.java
+++ src/main/java/com/example/demo/dto/AuthRequest.java
@@ -1,39 +1,13 @@
-// package com.example.demo.dto;
-
-// import lombok.Data;
-
-// @Data
-// public class AuthRequest {
-//     private String email;
-//     private String password;
-// }
 package com.example.demo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthRequest {
-
- private String email;
- private String password;
-
- public AuthRequest() {}
-
- public AuthRequest(String email, String password) {
-  this.email = email;
-  this.password = password;
- }
-
- public String getEmail() {
-  return email;
- }
-
- public void setEmail(String email) {
-  this.email = email;
- }
-
- public String getPassword() {
-  return password;
- }
-
- public void setPassword(String password) {
-  this.password = password;
- }
+    private String email;
+    private String password;
 }
