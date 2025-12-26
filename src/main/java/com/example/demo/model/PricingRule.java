f68cdd8da8d8ea3b2a39ec6c65cee94e5bd9a36f
bala-amypo <bala@amypo.in>
2025-12-26 14:03:33 +0000

    User commit

--- src/main/java/com/example/demo/model/PricingRule.java
+++ src/main/java/com/example/demo/model/PricingRule.java
@@ -1,36 +1,61 @@
+// // package com.example.demo.model;
+
+// // import jakarta.persistence.*;
+// // import lombok.Data;
+// // import lombok.AllArgsConstructor;
+// // import lombok.NoArgsConstructor;
+// // import jakarta.validation.constraints.Positive;
+
+// // import jakarta.persistence.ManyToOne;
+// // import jakarta.persistence.JoinColumn;
+// // import com.example.demo.model.EventRecord;
+
+// // @Entity
+// // @Data
+// // @AllArgsConstructor
+// // @NoArgsConstructor
+// // public class PricingRule {
+
+// //     @Id
+// //     @GeneratedValue(strategy = GenerationType.IDENTITY)
+// //     private Long id;
+// //     @Column(unique=true)
+// //     private String ruleCode;
+// //     private String description;
+// //     private Integer minRemainingSeats;
+// //     private Integer maxRemainingSeats;
+// //     private Integer daysBeforeEvent;
+// //     @Positive
+// //     private Double priceMultiplier;
+// //     private Boolean active= true;
+// //     @ManyToOne
+// //     @JoinColumn(name = "event_id")
+// //     private EventRecord event;
+// // }
 // package com.example.demo.model;
 
 // import jakarta.persistence.*;
-// import lombok.Data;
-// import lombok.AllArgsConstructor;
-// import lombok.NoArgsConstructor;
-// import jakarta.validation.constraints.Positive;
+// import lombok.Getter;
+// import lombok.Setter;
 
-// import jakarta.persistence.ManyToOne;
-// import jakarta.persistence.JoinColumn;
-// import com.example.demo.model.EventRecord;
-
+// @Getter
+// @Setter
 // @Entity
-// @Data
-// @AllArgsConstructor
-// @NoArgsConstructor
 // public class PricingRule {
 
 //     @Id
 //     @GeneratedValue(strategy = GenerationType.IDENTITY)
 //     private Long id;
-//     @Column(unique=true)
+
 //     private String ruleCode;
 //     private String description;
+
+//     private Integer daysBeforeEvent;
 //     private Integer minRemainingSeats;
 //     private Integer maxRemainingSeats;
-//     private Integer daysBeforeEvent;
-//     @Positive
+
 //     private Double priceMultiplier;
-//     private Boolean active= true;
-//     @ManyToOne
-//     @JoinColumn(name = "event_id")
-//     private EventRecord event;
+//     private Boolean active;
 // }
 package com.example.demo.model;
 
--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,12 +1,41 @@
+// package com.example.demo.service.impl;
+
+// import com.example.demo.model.User;
+// import com.example.demo.repository.UserRepository;
+// import com.example.demo.service.UserService;
+// import com.example.demo.exception.BadRequestException;
+// import org.springframework.stereotype.Service;
+
+// import java.util.List;
+
+// @Service
+// public class UserServiceImpl implements UserService {
+
+//     private final UserRepository userRepository;
+
+//     public UserServiceImpl(UserRepository userRepository) {
+//         this.userRepository = userRepository;
+//     }
+
+//     @Override
+//     public Optional<User> findByEmail(String email) {
+//         return userRepository.findByEmail(email);
+//     }
+
+//     @Override
+//     public boolean existsByEmail(String email) {
+//         return userRepository.existsByEmail(email);
+//     }
+// }
 package com.example.demo.service.impl;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
 
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
-import com.example.demo.exception.BadRequestException;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
