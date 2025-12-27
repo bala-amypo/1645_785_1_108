26e274d2bb0e6a4c99fed75f4465dfb157030c74
bala-amypo <bala@amypo.in>
2025-12-27 01:18:40 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,37 +1,37 @@
-// package com.example.demo.controller;
+package com.example.demo.controller;
 
-// import org.springframework.web.bind.annotation.*;
-// import java.util.List;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
-// import com.example.demo.model.User;
-// import com.example.demo.service.UserService;
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
 
-// @RestController
-// public class UserController {
+@RestController
+public class UserController {
 
-//     private final UserService service;
+    private final UserService service;
 
-//     public UserController(UserService service) {
-//         this.service = service;
-//     }
+    public UserController(UserService service) {
+        this.service = service;
+    }
 
-//     @PostMapping("/users/")
-//     public User createUser(@RequestBody User user) {
-//         return service.save(user);
-//     }
+    @PostMapping("/users/")
+    public User createUser(@RequestBody User user) {
+        return service.save(user);
+    }
 
-//     @GetMapping("/users/{id}")
-//     public User getUserById(@PathVariable Long id) {
-//         return service.getUserById(id);
-//     }
+    @GetMapping("/users/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return service.getUserById(id);
+    }
 
-//     @GetMapping("/users/")
-//     public List<User> getAllUsers() {
-//         return service.getAllUsers();
-//     }
+    @GetMapping("/users/")
+    public List<User> getAllUsers() {
+        return service.getAllUsers();
+    }
 
-//     @GetMapping("/users/email/{email}")
-//     public User getUserByEmail(@PathVariable String email) {
-//         return service.findByEmail(email);
-//     }
-// }
+    @GetMapping("/users/email/{email}")
+    public User getUserByEmail(@PathVariable String email) {
+        return service.findByEmail(email);
+    }
+}
