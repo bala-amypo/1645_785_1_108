cef7e5fb28f0e7fdc31bae45b08cd983de95329c
bala-amypo <bala@amypo.in>
2025-12-29 06:08:23 +0000

    User commit

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -1,24 +1,24 @@
-package com.example.demo.security;
+// package com.example.demo.security;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-public class SecurityConfig {
+// @Configuration
+// public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//     @Bean
+//     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                )
-                .formLogin(login -> login.disable())
-                .httpBasic(basic -> basic.disable());
+//         http
+//                 .csrf(csrf -> csrf.disable())
+//                 .authorizeHttpRequests(auth -> auth
+//                         .anyRequest().permitAll()
+//                 )
+//                 .formLogin(login -> login.disable())
+//                 .httpBasic(basic -> basic.disable());
 
-        return http.build();
-    }
-}
+//         return http.build();
+//     }
+// }
