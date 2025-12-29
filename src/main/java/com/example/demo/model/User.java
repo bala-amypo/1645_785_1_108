b4fddc9a646ffe1f2a495b5e964e29e03a06da22
bala-amypo <bala@amypo.in>
2025-12-29 06:00:00 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -28,36 +28,57 @@
 //     @NotBlank
 //     private String role;
 // }
+// package com.example.demo.model;
+
+// import lombok.Data;
+// import jakarta.persistence.*;
+// @Data
+// @Entity
+// @Table(name = "users")
+// public class User {
+
+//  @Id
+//  @GeneratedValue(strategy = GenerationType.IDENTITY)
+//  private Long id;
+
+//  private String fullName;
+
+//  @Column(unique = true)
+//  private String email;
+
+//  private String password;
+
+//  private String role;
+
+//  public User() {}
+
+//  public User(Long id, String fullName, String email,
+//  String password, String role) {
+//   this.id = id;
+//   this.fullName = fullName;
+//   this.email = email;
+//   this.password = password;
+//   this.role = role;
+//  }
+// }
 package com.example.demo.model;
 
-import lombok.Data;
-import jakarta.persistence.*;
-@Data
-@Entity
-@Table(name = "users")
 public class User {
 
- @Id
- @GeneratedValue(strategy = GenerationType.IDENTITY)
- private Long id;
+    private Long id;
+    private String email;
+    private String password;
+    private String role;
 
- private String fullName;
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
- @Column(unique = true)
- private String email;
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
- private String password;
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 
- private String role;
-
- public User() {}
-
- public User(Long id, String fullName, String email,
- String password, String role) {
-  this.id = id;
-  this.fullName = fullName;
-  this.email = email;
-  this.password = password;
-  this.role = role;
- }
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
 }
